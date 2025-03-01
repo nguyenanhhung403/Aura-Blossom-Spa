d21dc2ed2b40c251ed143eb7b2c5e9c8feb628a5
Nguyen Anh Hung <nguyenanhhung403@gmail.com>
2025-03-01 22:39:35 +0700

    fix folder, add email check

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { UserProvider } from './context/UserContext';
+import { UserProvider } from './components/context/UserContext';
 import './App.css';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
-import Home from './pages/Home';
+import Home from './components/pages/Home';
 import ForgottenPW from './components/Auth/ForgottenPW';
-import Blog from './pages/Blogs';
+import Blog from './components/pages/Blogs';
 import EmailCheck from './components/Auth/EmailCheck';
-import Profile from './pages/Profile';
+import Profile from './components/pages/Profile';
+import BookingProcess from './components/pages/BookingProcess';
 function App() {
     return (
         <UserProvider>
             <Router>
                 <Routes>
+                    <Route path='/booking' element={<BookingProcess/>}/>
                     <Route path="/profile" element={<Profile />} />
                     <Route path="/email-check" element={<EmailCheck />} />
                     <Route path="/" element={<Home />} />
