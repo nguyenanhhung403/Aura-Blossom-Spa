cf48a867ab9432872f9d4e1ea64ac8825ae3415c
Trương Thị Thanh Huyền <ht79247@gmail.com>
2025-03-08 11:59:37 +0700

    Lưu thay đổi trước khi merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-import Login from './components/Login';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { UserProvider } from './context/UserContext';
+import { UserProvider } from './components/context/UserContext';
 import './App.css';
-import Register from './components/Register';
-import Home from './pages/Home';
-import ForgottenPW from './components/ForgottenPW';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import Home from './components/pages/Home';
+import ForgottenPW from './components/Auth/ForgottenPW';
+import Blog from './components/pages/Blogs';
+import EmailCheck from './components/Auth/EmailCheck';
+import Profile from './components/pages/Profile';
+import BookingProcess from './components/pages/BookingProcess';
 function App() {
     return (
         <Router>
             <UserProvider>
                 <Routes>
+                    <Route path='/booking' element={<BookingProcess/>}/>
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/email-check" element={<EmailCheck />} />
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
-                    <Route path="/forgot" element={<ForgottenPW />} />
+                    <Route path="/forgot" element={<ForgottenPW />} />             <Route path="/blog" element={<Blog />} />
                 </Routes>
             </UserProvider>
         </Router>
     );
 }
+
 export default App;
