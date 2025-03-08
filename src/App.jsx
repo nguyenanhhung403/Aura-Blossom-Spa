b40ad966743843d0491ca580e2d772816cd6dc52
Nguyen Anh Hung <nguyenanhhung403@gmail.com>
2025-03-08 20:02:56 +0700

    Bo sung router Admin

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,17 @@ import Blog from './components/pages/Blogs';
 import EmailCheck from './components/Auth/EmailCheck';
 import Profile from './components/pages/Profile';
 import BookingProcess from './components/pages/BookingProcess';
+import AminDashboard from './components/pages/Admin/AdminDashboard';
+import ServiceManagement from './components/pages/Admin/ServiceManagement';
+import AppointmentManagement from './components/pages/Admin/AppointmentManagement';
+import StaffManagerment from './components/pages/Admin/StaffManagement';
+import StaffList from './components/pages/Admin/stafflist';
+import TherapistList from './components/pages/Admin/therapistList';
+import CustomerList from './components/pages/Admin/CustomerList';
+import Schedule from './components/pages/Admin/Schedules';
+import BlogList from './components/pages/Admin/BlogsList';
+import FeedbackList from './components/pages/Admin/FeedBackList';
+import SpaDashboard from './components/pages/Admin/SpaDashboard';
 function App() {
     return (
         <UserProvider>
@@ -23,6 +34,17 @@ function App() {
                     <Route path="/register" element={<Register />} />
                     <Route path="/forgot" element={<ForgottenPW />} />
                     <Route path="/blog" element={<Blog />} />
+                    <Route path="/admin/*" element={<AminDashboard />} />
+                    <Route path="/admin/services" element={<ServiceManagement />} />
+                    <Route path="/admin/appointments" element={<AppointmentManagement />} />
+                    <Route path="/admin/employees" element={<StaffManagerment />} />
+                    <Route path="/admin/employees/staff" element={<StaffList />} />
+                    <Route path="/admin/employees/specialists" element={<TherapistList />} />
+                    <Route path="/admin/customers" element={<CustomerList />} />
+                    <Route path="/admin/schedules" element={<Schedule />} />
+                    <Route path="/admin/blogs" element={<BlogList />} />
+                    <Route path="/admin/feedback" element={<FeedbackList />} />
+                    <Route path="/admin/dashboard" element={<SpaDashboard />} />
                 </Routes>
             </Router>
         </UserProvider>
